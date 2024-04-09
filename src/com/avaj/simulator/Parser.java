ec7aca1d55e536b459542cb146349d4a43fc297b
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-04-09 17:57:03 +0200

    begin parsing

--- src/com/avaj/simulator/Parser.java
+++ src/com/avaj/simulator/Parser.java
@@ -1,13 +1,53 @@
 package src.com.avaj.simulator;
 
 import src.com.avaj.exception.AvajLauncherException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.NumberFormatException;
+import java.util.Scanner;
 
 public class Parser {
 
     private int nb_iteration;
 
-    public void Parser(){
-        java.util.Scanner reader;
+    private void checkArgs(String[] args) throws AvajLauncherException {
+        if (args.length != 1)
+            throw new AvajLauncherException("Invalid number of parameters");
+    }
 
+    private File loadFile(String arg) throws AvajLauncherException {
+        File file = new File(arg);
+        if (!file.exists())
+            throw new AvajLauncherException("Failed to open file");
+        return file;
+    }
+
+    private int loadNbIteration(Scanner reader) throws AvajLauncherException {
+        try {
+            String line = reader.nextLine();
+            return Integer.parseInt(line);
+        }
+        catch (NumberFormatException e) {
+            throw new AvajLauncherException("Number of iterations need integer value");
+        }
+    }
+
+    private void printDebug() {
+        System.out.println("NUMBER OF ITERATIONS :  " + this.nb_iteration);
+    }
+
+    private void readFile(File file) throws AvajLauncherException {
+        try (Scanner reader = new Scanner(file)) {
+            this.nb_iteration = loadNbIteration(reader);
+            printDebug();
+        } catch (FileNotFoundException e) {
+            throw new AvajLauncherException("Parsing error");
+        }
+    }
+
+    public Parser(String[] args) throws AvajLauncherException {
+        checkArgs(args);
+        File file = loadFile(args[0]);
+        readFile(file);
     }
 }
--- src/com/avaj/simulator/Simulator.java
+++ src/com/avaj/simulator/Simulator.java
@@ -6,11 +6,12 @@ import src.com.avaj.exception.*;
 public class Simulator {
     public static void main(String[] args) throws AvajLauncherException {
         try {
+            Parser parser = new Parser(args);
 
-            Coordinates coord = new Coordinates(100,50,100);
-            for (int i = 0; i < 5; i++) {
-                AircraftFactory.newAircraft("Helicopter", "BS " + i, coord);
-            }
+            // Coordinates coord = new Coordinates(100,50,100);
+            // for (int i = 0; i < 5; i++) {
+            //     AircraftFactory.newAircraft("Helicopter", "BS " + i, coord);
+            // }
         }
         catch (AvajLauncherException e) {
             System.out.println("AvajLauncher: " + e.getMessage());
