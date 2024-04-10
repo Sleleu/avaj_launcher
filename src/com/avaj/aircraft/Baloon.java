192c3b1956af820900efe17847000b951d68408f
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-04-10 13:21:45 +0200

    add Baloon and JetPlane

---
+++ src/com/avaj/aircraft/Baloon.java
@@ -0,0 +1,37 @@
+package src.com.avaj.aircraft;
+
+public class Baloon extends Aircraft {
+    public Baloon(long p_id, String p_name, Coordinates p_coordinate) {
+        super(p_id, p_name, p_coordinate);
+    }
+
+    @Override
+    public void updateConditions() {
+
+        String weather = this.weatherTower.getWeather(this.coordinates);
+        String message = "";
+        switch(weather) {
+            case "SUN":
+                message = "Let's enjoy the good weather and take some pics.";
+                this.coordinates.setLongitude(this.coordinates.getLongitude() + 2);
+                this.coordinates.setHeight(this.coordinates.getHeight() + 4);
+                break;
+            case "RAIN":
+                message = "Damn you rain! You messed up my baloon.";
+                this.coordinates.setHeight(this.coordinates.getHeight() - 5);
+                break;
+            case "FOG":
+                message = "We can't keep flying in this fog. We're gonna crash.";
+                this.coordinates.setHeight(this.coordinates.getHeight() - 3);
+                break;
+            case "SNOW":
+                message = "It's snowing. We're gonna crash.";
+                this.coordinates.setHeight(this.coordinates.getHeight() - 15);
+                break;
+        }
+        String identity = this.getClass().getSimpleName() + "#" + this.getName() + "(" + this.getId() + ")";
+        System.out.println(identity + ": " + message);
+        super.updateConditions();
+    }
+
+}
---
+++ src/com/avaj/aircraft/JetPlane.java
@@ -0,0 +1,36 @@
+package src.com.avaj.aircraft;
+
+public class JetPlane extends Aircraft {
+    public JetPlane(long p_id, String p_name, Coordinates p_coordinate) {
+        super(p_id, p_name, p_coordinate);
+    }
+
+    @Override
+    public void updateConditions() {
+
+        String weather = this.weatherTower.getWeather(this.coordinates);
+        String message = "";
+        switch(weather) {
+            case "SUN":
+                message = "Careful of the sunburn!";
+                this.coordinates.setLatitude(this.coordinates.getLatitude() + 10);
+                this.coordinates.setHeight(this.coordinates.getHeight() + 2);
+                break;
+            case "RAIN":
+                message = "It's raining. Better watch out for lightings.";
+                this.coordinates.setLatitude(this.coordinates.getLatitude() + 5);
+                break;
+            case "FOG":
+                message = "I want to fly above the fog!";
+                this.coordinates.setLatitude(this.coordinates.getLatitude() + 1);
+                break;
+            case "SNOW":
+                message = "OMG! Winter is coming!";
+                this.coordinates.setHeight(this.coordinates.getHeight() - 7);
+                break;
+        }
+        String identity = this.getClass().getSimpleName() + "#" + this.getName() + "(" + this.getId() + ")";
+        System.out.println(identity + ": " + message);
+        super.updateConditions();
+    }
+}
